702887ad1e532a71448dce217283f6429d3ab4dc
Kai Yang <65525083+kaiyang7766@users.noreply.github.com>
2021-04-05 22:40:53 +0800

    Update input.js

--- public/js/input.js
+++ public/js/input.js
@@ -10,10 +10,24 @@ export function setupKeyboard(entity){
 			entity.jump.cancel();
 		}
 	});
+	input.addMapping('KeyW', keyState=>{
+		if (keyState){
+			entity.jump.start();
+		} 
+		else{
+			entity.jump.cancel();
+		}
+	});
 	input.addMapping('ArrowRight', keyState=>{
 		entity.go.dir+=keyState?1:-1;
 	});
+	input.addMapping('KeyD', keyState=>{
+		entity.go.dir+=keyState?1:-1;
+	});
 	input.addMapping('ArrowLeft', keyState=>{
+		entity.go.dir+=keyState?-1:1;
+	});
+	input.addMapping('KeyA', keyState=>{
 		entity.go.dir+=keyState?-1:1;
 	});
 
